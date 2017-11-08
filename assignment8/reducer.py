0a071e6720b8b45b5d34431b66a6fd096c5c514d
Sazidul Islam <bsse0633@iit.du.ac.bd>
2017-11-08 01:51:12 -0500

    assignment 08 done

---
+++ assignment8/reducer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import sys
+hitsTotal = 0
+oldKey = None
+maxHits = 0
+maxPath = None
+
+for line in sys.stdin:
+    thisKey = line.strip()
+    if oldKey and oldKey != thisKey:
+        if hitsTotal > maxHits:
+            maxHits = hitsTotal
+            maxPath = oldKey
+        oldKey = thisKey;
+        hitsTotal = 0
+
+    oldKey = thisKey
+    hitsTotal += 1
+
+if oldKey != None:
+    if hitsTotal > maxHits:
+		maxHits = hitsTotal
+        maxPath = oldKey
+print maxPath+maxHits
